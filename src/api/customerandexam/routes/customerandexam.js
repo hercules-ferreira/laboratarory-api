62733180013546ac5d50d0cbe924b3a81f5d255b
Hercules Ferreira <olcaminote82019@gmail.com>
2023-04-20 20:13:23 -0300

    conclusão do projeto v1

---
+++ src/api/customerandexam/routes/customerandexam.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customerandexam router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::customerandexam.customerandexam');
---
+++ src/api/customerandexam/services/customerandexam.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customerandexam service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::customerandexam.customerandexam');
